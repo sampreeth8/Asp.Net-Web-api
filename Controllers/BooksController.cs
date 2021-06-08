75b7fb2b60e903866fa06867b1a1e257fb4b9f48
Sampreeth <55551@hexaware.com>
2021-06-08 11:00:07 +0530

    Intial web api commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+     
+        public BooksService _booksService;
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpGet("get-allbooks")]
+        public IActionResult GetAllBooks()
+        {
+            var books = _booksService.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("get-bookbyid/{Id}")]
+        public IActionResult GetBookById(int Id)
+        {
+            var book = _booksService.GetBookById(Id);
+            if (book == null)
+            {
+                return NotFound("Did not found any book with given book id");
+            }
+            return Ok(book);
+        }
+
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook(BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-bookbyid/{id}")]
+        public IActionResult UpdateBookById(int id,[FromBody] BookVM book)
+        {
+            var _book = _booksService.UpdateBookById(id, book);
+            return Ok(_book);
+        }
+
+        [HttpDelete("delete-bookbyid/{id}")]
+
+        public IActionResult DeleteBookById(int id)
+        {
+            var result = _booksService.DeleteBookById(id);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Data/Services/BooksService.cs
@@ -0,0 +1,86 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead?book.Rate.Value:null,
+                Genre = book.Genre,
+                CoverUrl = book.CoverUrl
+                
+
+            };
+
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public Book GetBookById(int Id)
+        {
+            var model = _context.Books.FirstOrDefault(p => p.Id == Id);
+            return model;
+        }
+
+        public Book UpdateBookById(int id,BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(p => p.Id == id);
+            if (_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.IsRead ? book.Rate.Value : null;
+                _book.Genre = book.Genre;
+                _book.CoverUrl = book.CoverUrl;
+
+                _context.SaveChanges();
+            }
+
+            return _book;
+        }
+
+        public string DeleteBookById(int id)
+        {
+            var book = _context.Books.FirstOrDefault(p => p.Id == id);
+            if(book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+
+                return "Succesfully deleted";
+
+            }
+            else
+            {
+                return "Cannot Find the book with given id";
+            }
+            
+        }
+    }
+}
